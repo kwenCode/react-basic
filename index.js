dc85483a26dd203eec16e3e63057f270071aa0f4
kwanhyo <lhc2472@gmail.com>
2023-10-07 21:30:38 +0900

    chapter3 end

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-//import {PI, getArea, getCircumference} from "./circle.js"; // 모듈값 불러오기
-//import * as circle from "./circle.js"; //전부 불러오기
-import circle from "./circle.js"; //기본값으로 내보내기
+import lodash from "lodash";
 
-//console.log(PI, getArea(1), getCircumference(1));
-console.log(circle.PI, circle.getArea(1), circle.getCircumference(1));
+const arr = [1, 1, 1, 2, 2, 1, 1, 4, 4, 3, 2]
+const uniqueArr = lodash.unionBy(arr);
+
+console.log(uniqueArr);
